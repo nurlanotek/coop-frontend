1d6c980725f9755d520aa9b3da63e9474c0d690b
Ulugbek Karimov <ulugbek.karimov@UCA.corp>
2022-03-10 15:58:25 +0600

    first committtttttttttttttt

--- js/main.js
+++ js/main.js
@@ -85,3 +85,57 @@ $(function() {
 	siteMenuClone();
 
 });
+
+
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+$(document).ready(function(){
+	$(window).scroll(function(){
+		var scroll = $(window).scrollTop();
+		if (scroll > 550) {
+		  $(".site-navbar .site-navigation").css("background-color" , "black");
+		}
+  
+		else{
+			$(".site-navbar .site-navigation").css("background-color" , "white");
+		}
+	})
+  })
+
+  $(document).ready(function(){
+	$(window).scroll(function(){
+		var scroll = $(window).scrollTop();
+		if (scroll > 550) {
+		  $(".site-navbar .site-navigation .site-menu > li > a").css("color" , "white");  	
+		}
+  
+		else{
+			$(".site-navbar .site-navigation .site-menu > li > a").css("color" , "black");  	
+		}
+	})
+  })
